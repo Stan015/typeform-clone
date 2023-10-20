cc03470e4034c9f95e2f3216c63e9643d639ebbf
Stanley Azi <azistanley17@gmail.com>
2023-10-20 20:43:01 +0100

    new commit

--- script.js
+++ script.js
@@ -1,24 +1,117 @@
-const navLink = document.querySelector(".products");
-const arrowDown = document.getElementById("nav-arrow-down");
-const navProduct = document.querySelector(".products-content");
+const arrowDown = document.querySelectorAll(".nav-arrow-down");
 
 const arrowRotate = () => {
-    navLink.addEventListener('mouseenter', function() {
-        arrowDown.classList.add("arrow-rotate");
+    //for nav products lists
+    const navProduct = document.querySelector(".products");
+    const navProductContents = document.querySelector(".products-content");
 
-        if (navProduct.style.display = "none") {
-            navProduct.style.display = "flex";
+    navProduct.addEventListener('mouseenter', function() {
+        arrowDown[0].classList.add("arrow-rotate");
+
+        if (navProductContents.style.display = "none") {
+            navProductContents.style.display = "flex";
         };
 
     });
 
-    navProduct.addEventListener('mouseleave', function() {
-        arrowDown.classList.remove("arrow-rotate");
+    navProductContents.addEventListener('mouseleave', function() {
+        arrowDown[0].classList.remove("arrow-rotate");
 
-        if (navProduct.style.display = "flex") {
-            navProduct.style.display = "none";
+        if (navProductContents.style.display = "flex") {
+            navProductContents.style.display = "none";
         };
     });
+    //
+
+    //fir solutions lists
+    const navSolutions = document.querySelector(".solutions");
+    const navSolutionContents = document.querySelector(".solutions-lists");
+
+    navSolutions.addEventListener('mouseenter', function() {
+        arrowDown[1].classList.add("arrow-rotate");
+
+        if (navSolutionContents.style.display = "none") {
+            navSolutionContents.style.display = "flex";
+        };
+
+    });
+
+    navSolutionContents.addEventListener('mouseleave', function() {
+        arrowDown[1].classList.remove("arrow-rotate");
+
+        if (navSolutionContents.style.display = "flex") {
+            navSolutionContents.style.display = "none";
+        };
+    });
+    //
+    
+    //for templates lists
+    const navTemplates = document.querySelector(".templates");
+    const navTemplatesContents = document.querySelector(".templates-lists");
+
+    navTemplates.addEventListener('mouseenter', function() {
+        arrowDown[2].classList.add("arrow-rotate");
+
+        if (navTemplatesContents.style.display = "none") {
+            navTemplatesContents.style.display = "flex";
+        };
+
+    });
+
+    navTemplatesContents.addEventListener('mouseleave', function() {
+        arrowDown[2].classList.remove("arrow-rotate");
+
+        if (navTemplatesContents.style.display = "flex") {
+            navTemplatesContents.style.display = "none";
+        };
+    });
+    //
+
+    //for integrations lists
+    const navIntegrations = document.querySelector(".integrations-pr-link");
+    const navIntegrationsContents = document.querySelector(".integrations-comps");
+
+    navIntegrations.addEventListener('mouseenter', function() {
+        arrowDown[3].classList.add("arrow-rotate");
+
+        if (navIntegrationsContents.style.display = "none") {
+            navIntegrationsContents.style.display = "grid";
+        };
+
+    });
+
+    navIntegrationsContents.addEventListener('mouseleave', function() {
+        arrowDown[3].classList.remove("arrow-rotate");
+
+        if (navIntegrationsContents.style.display = "grid") {
+            navIntegrationsContents.style.display = "none";
+        };
+    });
+    //
+
+    //for Resources and support lists
+    const navResources = document.querySelector(".r-and-s");
+    const navResourcesContents = document.querySelector(".resources-and-support");
+
+    navResources.addEventListener('mouseenter', function() {
+        arrowDown[4].classList.add("arrow-rotate");
+
+        if (navResourcesContents.style.display = "none") {
+            navResourcesContents.style.display = "flex";
+        };
+
+    });
+
+    navResourcesContents.addEventListener('mouseleave', function() {
+        arrowDown[4].classList.remove("arrow-rotate");
+
+        if (navResourcesContents.style.display = "flex") {
+            navResourcesContents.style.display = "none";
+        };
+    });
+    //
 }
+
 arrowRotate();
+//
 
